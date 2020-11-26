45f68f592ea54e1bd7c78ad49285046eead89cd6
Daniel Freitas Ferreira <72029912+Daniel-f-ferreira@users.noreply.github.com>
2020-11-26 21:03:29 +0000

    Add files via upload

---
+++ ContinuousAssessment2_GroupWork.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package continuous.assessment.pkg2_.group.work;
+
+/**
+ *
+ * @author timetask_group_h
+ */
+public class ContinuousAssessment2_GroupWork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("Please enter some text");  // This command line will print on screen a message;
+    }
+    
+}
